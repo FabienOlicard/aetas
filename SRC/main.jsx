c8122e1cc77c435a90841d5e68ec022b375c1539
FabienOlicard <fabien.olicard@me.com>
2026-06-09 13:06:07 +0200

    Delete SRC/main.jsx

--- SRC/main.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import AlbertAgeApp from './AlbertAgeApp.jsx';
-import './index.css';
-
-createRoot(document.getElementById('root')).render(<AlbertAgeApp />);
